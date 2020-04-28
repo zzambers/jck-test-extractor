58e88076038c6ae3a8268b37381a7cba708ff5fc
Zdenek Zambersky <zzambers@redhat.com>
2020-04-28 21:05:24 +0200

    TestMakefile.mk: improvements in native code handeling

--- src/main/resources/jckextractor/res/TestMakefile.mk
+++ src/main/resources/jckextractor/res/TestMakefile.mk
@@ -3,12 +3,13 @@ JAVAC=$(JAVA_HOME)/bin/javac
 #CC=gcc
 
 SCRIPTS=$(shell find tests -type f -name '*.ksh' )
+LIBS=$(shell [ -d src/share/lib/jni ] && echo 'lib/libjckjni.so' ; [ -d src/share/lib/jvmti ] && echo 'lib/libjckjvmti.so' )
 
 .PHONY: test clean $(SCRIPTS)
 
 test: $(SCRIPTS)
 
-$(SCRIPTS): %.ksh: | classes lib
+$(SCRIPTS): %.ksh: | classes $(LIBS)
 	export LD_LIBRARY_PATH=$$(pwd)/lib ; \
 	sh "$@"
 
@@ -18,8 +19,12 @@ classes:
 
 lib:
 	mkdir lib
-	! [ -f src/share/lib/jni/jckjni.c ] || $(CC) -fPIC -shared -Isrc/share/lib/jni/include -Isrc/share/lib/jni/include/amd64 -o lib/libjckjni.so src/share/lib/jni/jckjni.c
-	! [ -f src/share/lib/jvmti/jckjvmti.c ] || $(CC) -fPIC -shared -I. -Isrc/share/lib/jvmti/include -Isrc/share/lib/jni/include/amd64 -Isrc/share/lib/jni/include -o lib/libjckjvmti.so src/share/lib/jvmti/jckjvmti.c
+
+lib/libjckjni.so: | lib
+	$(CC) -fPIC -shared -Isrc/share/lib/jni/include -Isrc/share/lib/jni/include/amd64 -o lib/libjckjni.so src/share/lib/jni/jckjni.c
+
+lib/libjckjvmti.so: | lib
+	$(CC) -fPIC -shared -I. -Isrc/share/lib/jvmti/include -Isrc/share/lib/jni/include/amd64 -Isrc/share/lib/jni/include -o lib/libjckjvmti.so src/share/lib/jvmti/jckjvmti.c
 
 clean:
 	rm -rf classes
